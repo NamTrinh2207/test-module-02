c5d25f538eda9ea82b0e8ed862c45f97421b2ae6
NamTrinh2207 <trinhnam180697@gmail.com>
2023-02-21 10:58:50 +0700

    test module 2

---
+++ studentManager/src/model/Student.java
@@ -0,0 +1,89 @@
+package model;
+
+import java.io.Serializable;
+
+public class Student implements Serializable,Comparable<Student> {
+    private String id;
+    private String fullName;
+    private int age;
+    private String gender;
+    private String address;
+    private double mediumScore;
+
+    public Student() {
+    }
+
+    public Student(String id, String fullName, int age, String gender, String address, double mediumScore) {
+        this.id = id;
+        this.fullName = fullName;
+        this.age = age;
+        this.gender = gender;
+        this.address = address;
+        this.mediumScore = mediumScore;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getMediumScore() {
+        return mediumScore;
+    }
+
+    public void setMediumScore(double mediumScore) {
+        this.mediumScore = mediumScore;
+    }
+
+    @Override
+    public String toString() {
+        return "Student " +
+                "id='" + id + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", address='" + address + '\'' +
+                ", mediumScore=" + mediumScore +
+                '\n';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return 0;
+    }
+}
